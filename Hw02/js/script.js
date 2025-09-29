b50594ec186541157c0141ede9eeff90b5e69422
bmo-crayola <uzamats@gmail.com>
2025-09-29 03:23:17 -0700

    Added a second GameMode

--- Hw02/js/script.js
+++ Hw02/js/script.js
@@ -24,35 +24,90 @@ console.log(currWord);
 let currMorse = toMorse();
 console.log(currMorse);
 
+let gameMode = 0;
+
 // Event listeners
+document.querySelector("#EngToMorse").addEventListener("click", EngToMorse);
+document.querySelector("#MorseToEng").addEventListener("click", MorseToEng);
+
 document.querySelector("#checkBtn").addEventListener("click", checkAnswer);
 document.querySelector("#giveUpBtn").addEventListener("click", giveUp)
 
-document.querySelector("#wordToGuess").textContent = currWord;
+
+function EngToMorse() {
+    document.querySelector("#wordToGuess").textContent = currWord;
+    document.querySelector("#prompt").textContent = "Type the Morse code for the word above using - and *";
+
+    document.querySelector("#EngToMorse").style.display = "none";
+    document.querySelector("#MorseToEng").style.display = "none";
+
+    document.querySelector("#checkBtn").removeAttribute("hidden");
+    document.querySelector("#giveUpBtn").removeAttribute("hidden");
+    document.querySelector("#morseInput").removeAttribute("hidden");
+    document.querySelector("#morseInput").placeholder = "Enter Morse code here (NO SPACES!)";
+}
+
+function MorseToEng() {
+    document.querySelector("#wordToGuess").textContent = currMorse;
+    document.querySelector("#prompt").textContent = "Type the English code for the word above using ABC";
+
+    document.querySelector("#EngToMorse").style.display = "none";
+    document.querySelector("#MorseToEng").style.display = "none";
+
+    document.querySelector("#checkBtn").removeAttribute("hidden");
+    document.querySelector("#giveUpBtn").removeAttribute("hidden");
+    document.querySelector("#morseInput").removeAttribute("hidden");
+    document.querySelector("#morseInput").placeholder = "Enter English letterse here (ALL UPPERCASE!)"
+
+    gameMode = 1;
+}
+
 
 function checkAnswer() {
     let morseInput = document.querySelector("#morseInput").value;
-    if (morseInput == currMorse) {
-        document.querySelector("#result").textContent="Correct!";
-        document.querySelector("#result").style.color="green";
-        document.querySelector("#wordToGuess").style.color="green";
-        document.querySelector("#resultImg").src = "img/check.png";
-    } else {
-        document.querySelector("#result").textContent="Wrong, try again!";
-        document.querySelector("#result").style.color="red";
-        document.querySelector("#wordToGuess").style.color="red";
-        document.querySelector("#resultImg").src = "img/cross.png";
+    if (gameMode == 0) {
+        if (morseInput == currMorse) {
+            document.querySelector("#result").textContent="Correct!";
+            document.querySelector("#result").style.color="green";
+            document.querySelector("#wordToGuess").style.color="green";
+            document.querySelector("#resultImg").src = "img/check.png";
+            document.querySelector("#resultImg").style.width = "5%"
+        } else {
+            document.querySelector("#result").textContent="Wrong, try again!";
+            document.querySelector("#result").style.color="red";
+            document.querySelector("#wordToGuess").style.color="red";
+            document.querySelector("#resultImg").src = "img/cross.png";
+            document.querySelector("#resultImg").style.width = "5%"
+        }
+    } else if (gameMode == 1) {
+        if (morseInput == currWord) {
+            document.querySelector("#result").textContent="Correct!";
+            document.querySelector("#result").style.color="green";
+            document.querySelector("#wordToGuess").style.color="green";
+            document.querySelector("#resultImg").src = "img/check.png";
+            document.querySelector("#resultImg").style.width = "5%"
+        } else {
+            document.querySelector("#result").textContent="Wrong, try again!";
+            document.querySelector("#result").style.color="red";
+            document.querySelector("#wordToGuess").style.color="red";
+            document.querySelector("#resultImg").src = "img/cross.png";
+            document.querySelector("#resultImg").style.width = "5%"
+        }
     }
 }
 
 function giveUp() {
     document.querySelector("#checkBtn").style.display = "none";
     // document.querySelector("#giveUpBtn").style.display = "none";
-
-    document.querySelector("#result").textContent = "Morse Code: " + currMorse;
+    if (gameMode = 0) {
+        document.querySelector("#result").textContent = "Morse Code: " + currMorse;
+    } else if (gameMode = 1) {
+        document.querySelector("#result").textContent = "English: " + currWord;
+    }
     document.querySelector("#result").style.color = "red";
     document.querySelector("#result").style.transform = "scale(1.33)"
     document.querySelector("#resultImg").src = "img/cross.png";
+    document.querySelector("#resultImg").style.width = "5%"
 }
 
 function toMorse() {
